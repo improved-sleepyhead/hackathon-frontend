247c66ca313c8e3c134838a4e32d9b5a31b0c967
hellpes <alexrui_hpow@mail.ru>
2025-05-26 10:53:01 +0300

    feat: add letter-preview

---
+++ src/app/tools/model/page.tsx
@@ -0,0 +1,9 @@
+import { LetterPreview } from '@/widgets/tools/letter-preview'
+
+export default function Page() {
+	return (
+		<div className="w-full h-full">
+			<LetterPreview />
+		</div>
+	)
+}
